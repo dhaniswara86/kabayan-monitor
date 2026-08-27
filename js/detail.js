1264db8cf6d41824e678858907b0f2a987bcd3d4
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-08-28 05:45:59 +0700

    Add files via upload

--- js/detail.js
+++ js/detail.js
@@ -1,32 +1,163 @@
+const params = new URLSearchParams(window.location.search);
+const id=params.get("id");
 
-const params=new URLSearchParams(location.search);
-const data=daftarBerkas.find(x=>x.id===params.get("id"));
+const data=daftarBerkas.find(x=>x.id===id);
+
 const container=document.getElementById("detailBerkas");
 
+
 if(data){
+
 document.getElementById("namaPerusahaan").innerHTML=data.perusahaan;
 document.getElementById("jenisBerkas").innerHTML=data.jenis;
 
+
 let timeline="";
+
 data.workflow.forEach(x=>{
+
+let icon="○";
+
+if(x.status==="selesai") icon="✓";
+if(x.status==="aktif") icon="●";
+if(x.status==="revisi") icon="↩";
+
+
 timeline+=`
+
 <div class="step">
-<strong>${x.status==="selesai"?"✓":x.status==="aktif"?"●":"○"} ${x.tahap}</strong>
+
+<div class="icon">${icon}</div>
+
+<div>
+
+<strong>${x.tahap}</strong>
+
 <p>${x.status}</p>
-</div>`;
+
+</div>
+
+</div>
+
+`;
+
 });
+
+
+let action="";
+
+
+if(currentUser.role==="Penyuluh Pajak" &&
+data.posisi==="Penyuluh Pajak"){
+
+action=`
+<button class="action"
+onclick="prosesApproval('${data.id}','approve')">
+Kirim Approval Kepala Seksi
+</button>
+`;
+
+}
+
+
+if(currentUser.role==="Kepala Seksi"){
+
+action=`
+
+<button class="approve"
+onclick="prosesApproval('${data.id}','approve')">
+✓ Setujui
+</button>
+
+
+<button class="reject"
+onclick="prosesApproval('${data.id}','return','Dokumen perlu dilengkapi')">
+↩ Kembalikan
+</button>
+
+`;
+
+}
+
+
+if(currentUser.role==="Kepala Kantor"){
+
+action=`
+
+<button class="approve"
+onclick="prosesApproval('${data.id}','approve')">
+✓ Approval Akhir
+</button>
+
+
+<button class="reject"
+onclick="prosesApproval('${data.id}','reject','Tidak disetujui')">
+✕ Tolak
+</button>
+
+`;
+
+}
+
 
 let history="";
-data.history.forEach(x=>{
-history+=`<div class="history">${x.tanggal}<br>${x.user}<br>${x.aksi}</div>`;
+
+data.history.forEach(h=>{
+
+history+=`
+
+<div class="history">
+
+<strong>${h.tanggal}</strong>
+
+<p>${h.user}</p>
+
+<span>${h.aksi}</span>
+
+${h.catatan ? `<small>${h.catatan}</small>`:""}
+
+</div>
+
+`;
+
 });
 
+
 container.innerHTML=`
+
 <div class="card">
+
 <h2>${data.perusahaan}</h2>
+
 <p>${data.nomorKasus}</p>
-<button class="action" onclick="lanjutkanWorkflow('${data.id}')">Lanjutkan Tahap</button>
+
+<p>
+Posisi:
+<strong>${data.posisi}</strong>
+</p>
+
+${action}
+
 </div>
-<div class="card"><h2>Perjalanan Berkas</h2>${timeline}</div>
-<div class="card"><h2>Riwayat Aktivitas</h2>${history}</div>`;
+
+
+<div class="card">
+
+<h2>Perjalanan Berkas</h2>
+
+${timeline}
+
+</div>
+
+
+<div class="card">
+
+<h2>Riwayat Aktivitas</h2>
+
+${history}
+
+</div>
+
+`;
+
 }
