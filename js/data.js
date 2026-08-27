cf7fceee9bf0837123f50696309810e3d57a8861
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-08-27 22:38:51 +0700

    Add initial berkas data model

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const daftarBerkas = [
+    {
+        id: "001",
+        nomorKasus: "C0014822877",
+        perusahaan: "PANFILA INDOSARI",
+        jenis: "PYSTT",
+        posisi: "Pelaksana",
+        progress: 10,
+        jatuhTempo: "2026-09-02",
+        status: "Dalam Proses"
+    },
+    {
+        id: "002",
+        nomorKasus: "SPTLB-2026-001",
+        perusahaan: "PT ABC INDONESIA",
+        jenis: "SPTLB Pendahuluan",
+        posisi: "Penyuluh Pajak",
+        progress: 50,
+        jatuhTempo: "2026-09-05",
+        status: "Dalam Proses"
+    },
+    {
+        id: "003",
+        nomorKasus: "REST-2026-002",
+        perusahaan: "PT XYZ",
+        jenis: "SPTLB Pemeriksaan",
+        posisi: "Approval Kepala Seksi",
+        progress: 70,
+        jatuhTempo: "2026-09-10",
+        status: "Review"
+    }
+];
