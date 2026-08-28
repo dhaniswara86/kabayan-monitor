9d5513cf383f0a759c575a5e01a4e2b9661becf7
dhaniswara86 <angga.dhaniswara@gmail.com>
2026-08-28 07:49:06 +0700

    buat supabase

---
+++ js/supabase.js
@@ -0,0 +1,11 @@
+// Konfigurasi Supabase
+
+const SUPABASE_URL = "https://atgctwjbdjmdebqrxjev.supabase.co/rest/v1/";
+
+const SUPABASE_KEY = "sb_publishable_OFi3MbjMqm443mepsFQGDQ_ViHnI0xM";
+
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
